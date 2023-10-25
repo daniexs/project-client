aa56284040ebcba57b6c9569d4c9388b0d003e55
daniexs <lggr404@gmail.com>
2023-10-25 23:30:14 +0700

    selesai fitur add

--- src/components/CardHome.vue
+++ src/components/CardHome.vue
@@ -5,21 +5,27 @@ import { useCounterStore } from '../stores/counter';
 export default {
     props: ['data'],
     methods: {
-        ...mapActions(useCounterStore,['addCart'])
+        ...mapActions(useCounterStore,['addCart']),
+        convertCur(value){
+            return new Intl.NumberFormat("id-ID", {
+                style: "currency",
+                currency: "IDR"
+            }).format(value)
+        }
     },
 }
 </script>
 <template>
     <div  class="rounded position-relative fruite-item">
-        <RouterLink :to="'/detail/'+ data.id" class="fruite-img" >
+        <div class="fruite-img" >
             <img :src="data.imgUrl" class="img-fluid w-100 rounded-top" alt="">
-        </RouterLink>
-        <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">Fruits</div>
+        </div>
+        <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">{{ data.category }}</div>
         <div class="p-4 border border-secondary border-top-0 rounded-bottom">
             <h4>{{ data.name }}</h4>
             <p>{{ data.description }}</p>
             <div class="d-flex justify-content-between flex-lg-wrap">
-                <p class="text-dark fs-5 fw-bold mb-0">$ {{ data.price }}</p>
+                <p class="text-dark fs-5 fw-bold mb-0"> {{ convertCur(data.price) }}</p>
                 <a href="#" v-on:click.prevent="addCart(data.id)" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
             </div>
         </div>
