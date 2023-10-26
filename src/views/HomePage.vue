cb526e61cf83eb82d1bc1e770111838505355070
daniexs <lggr404@gmail.com>
2023-10-26 10:24:55 +0700

    selsaii home

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,7 +9,10 @@ export default {
         ...mapActions(useCounterStore, ['fetchProducts']),
     },
     computed: {
-        ...mapState(useCounterStore, ['dataProducts'])
+        ...mapState(useCounterStore, ['dataProducts']),
+        getProduct(){
+            return this.dataProducts.slice(0,4)
+        }
     },
     created(){
         this.fetchProducts(1,'all')
@@ -45,10 +48,7 @@ export default {
                     <div class="col-md-12 col-lg-7">
                         <h4 class="mb-3 text-secondary">100% Organic Foods</h4>
                         <h1 class="mb-5 display-3 text-primary">Organic Veggies & Fruits Foods</h1>
-                        <div class="position-relative mx-auto">
-                            <input class="form-control border-2 border-secondary w-75 py-3 px-4 rounded-pill" type="number" placeholder="Search">
-                            <button type="submit" class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-white h-100" style="top: 0; right: 25%;">Submit Now</button>
-                        </div>
+                        
                     </div>
                     <div class="col-md-12 col-lg-5">
                         <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
@@ -171,7 +171,7 @@ export default {
                             <div class="row g-4">
                                 <div class="col-lg-12">
                                     <div class="row g-4">
-                                        <div class="col-md-6 col-lg-4 col-xl-3" v-for="(el, index) in dataProducts" >
+                                        <div class="col-md-6 col-lg-4 col-xl-3" v-for="(el, index) in getProduct" >
                                             <CardHome :data="el" />
                                         </div>
                                     </div>
